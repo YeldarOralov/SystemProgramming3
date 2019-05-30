4affd2a1a906a4f6b7b57444ea4cd295b0c6a8d3
YeldarOralov <YeldarOralov@users.noreply.github.com>
2019-05-30 11:52:14 +0600

    Добавлены файлы проекта

---
+++ ThreadHomeWork/ThreadHomeWork/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Media;
+using Microsoft.Win32;
+using System.Windows.Threading;
+using System.Threading;
+
+namespace ThreadHomeWork
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private MediaPlayer mediaPlayer;
+        Thread daemonMusic;
+        public MainWindow()
+        {
+            InitializeComponent();
+            mediaPlayer = new MediaPlayer();
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "MP3 files (*.mp3)|*.mp3|All files (*.*)|*.*";
+            if (openFileDialog.ShowDialog() == true)
+                mediaPlayer.Open(new Uri(openFileDialog.FileName));
+
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += timer_Tick;
+            timer.Start();
+        }
+
+        private void playMusicButtonClick(object sender, RoutedEventArgs e)
+        {
+            daemonMusic = new Thread(PlayMusic);
+            daemonMusic.IsBackground = true;
+            daemonMusic.Start();
+        }
+
+        private void PlayMusic()
+        {
+            Dispatcher.BeginInvoke(new ThreadStart(delegate { mediaPlayer.Play(); }));
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            if (mediaPlayer.Source != null)
+                musicLabel.Content = mediaPlayer.Position.ToString(@"mm\:ss");
+            else
+                musicLabel.Content = "No file selected...";
+        }
+    }
+}
---
+++ ThreadLesson/ThreadLesson/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ThreadLesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //PrintNumbers();
+
+            //Thread[] threads = new Thread[20];
+            //for (int i = 0; i < threads.Length; i++)
+            //{
+            //    threads[i] = new Thread(new ThreadStart(PrintNumbers));
+            //}
+            //for (int i = 0; i < threads.Length; i++)
+            //{
+            //    threads[i].Start();
+            //}
+
+
+            var daemon = new Thread(sumArguments=> {
+                Thread.Sleep(10000);
+                var arguments = sumArguments as SumArguments;
+                Console.WriteLine(arguments.X + arguments.Y);
+            });
+            daemon.IsBackground = false;
+            daemon.Start(new SumArguments { X = 1, Y = 5 });
+
+            //Console.ReadLine();
+
+        }
+
+        private static void PrintNumbers()
+        {
+            var currentThread = Thread.CurrentThread;
+            Console.WriteLine($"***Поток с Id {currentThread.ManagedThreadId} начал работу***");
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write(i + " ");
+                Thread.Sleep(100);
+            }
+
+            Console.WriteLine($"\n***Поток с Id {currentThread.ManagedThreadId} закончил работу***");
+        }
+
+        private static void Sum(object sumArguments)
+        {
+            Thread.Sleep(10000);
+            var arguments = sumArguments as SumArguments;
+            Console.WriteLine(arguments.X + arguments.Y);
+        }
+    }
+}
